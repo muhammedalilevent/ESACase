ea72deb33f73a91df53ce47757f7c73d714d7c6e
muhammedalilevent <technologiesssw@gmail.com>
2023-07-13 00:46:37 +0300

    DetailFragment updates TCKN Validation Added Material Added

--- app/src/main/java/com/levent/customerinstallations/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/levent/customerinstallations/presentation/detail/DetailFragment.kt
@@ -1,7 +1,11 @@
 package com.levent.customerinstallations.presentation.detail
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.View
+import androidx.compose.ui.graphics.Color
+import androidx.core.content.ContextCompat
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.levent.customerinstallations.R
@@ -16,5 +20,130 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setUIContents()
+
+    }
+
+    private fun setUIContents() {
+        with(binding.tilNameSurname) {
+            this.editText?.toString()
+            this.editText?.doOnTextChanged { inputText, _, _, _ ->
+                println("45ss tc $inputText")
+            }
+        }
+
+        binding.tietNameSurname.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.tilNameSurname.isHelperTextEnabled = false
+            }
+        }
+        with(binding.tilTCKN) {
+            this.editText?.toString()
+            this.editText?.doOnTextChanged { inputText, _, _, _ ->
+                if (inputText?.length!! == 11) {
+                    if (isTCKNCorrect(inputText.toString())) {
+                        this.helperText = "TC Kimlik Doğrulaması Başarılı"
+
+                        this.setHelperTextColor(
+                            makeColorCSL(
+                                ContextCompat.getColor(
+                                    context,
+                                    R.color.colorVerified
+                                )
+                            )
+                        )
+                        this.boxStrokeColor = ContextCompat.getColor(
+                            context,
+                            R.color.colorVerified
+                        )
+                        this.hintTextColor = makeColorCSL(
+                            ContextCompat.getColor(
+                                context,
+                                R.color.colorVerified
+                            )
+                        )
+                    } else {
+                        this.error = "TC Kimlik Doğrulaması Başarısız"
+                    }
+                } else {
+                    resetTCKNInputState()
+                }
+            }
+        }
+
+        binding.tietTCKN.setOnFocusChangeListener { _, hasFocus ->
+            binding.tilNameSurname.isHelperTextEnabled = !hasFocus
+        }
+
+        with(binding.tilEmail) {
+            this.editText?.toString()
+            this.editText?.doOnTextChanged { inputText, _, _, _ ->
+                println("45ss tc $inputText")
+            }
+        }
+
+        binding.tietEmail.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.tilNameSurname.isHelperTextEnabled = false
+            }
+        }
+
+        with(binding.tilPhoneNumber) {
+            this.editText?.toString()
+            this.editText?.doOnTextChanged { inputText, _, _, _ ->
+                println("45ss tc $inputText")
+            }
+        }
+
+        binding.tietPhoneNumber.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.tilNameSurname.isHelperTextEnabled = false
+            }
+        }
+
+    }
+
+    private fun resetTCKNInputState() {
+        with(binding.tilTCKN) {
+            this.hintTextColor = makeColorCSL(
+                ContextCompat.getColor(
+                    context,
+                    R.color.colorPrimary
+                )
+            )
+            this.boxStrokeColor = ContextCompat.getColor(
+                context,
+                R.color.colorPrimary
+            )
+            this.setHelperTextColor(
+                makeColorCSL(
+                    ContextCompat.getColor(
+                        context,
+                        com.google.android.material.R.color.mtrl_indicator_text_color
+                    )
+                )
+            )
+            this.helperText = getString(R.string.helperTextTCKN)
+        }
+    }
+
+    private fun makeColorCSL(colorHashCode: Int): ColorStateList {
+        return ColorStateList.valueOf(colorHashCode)
+    }
+
+    private fun isTCKNCorrect(id: String?): Boolean {
+        if (id == null) return false
+        if (id.length != 11) return false
+        val chars = id.toCharArray()
+        val a = IntArray(11)
+        for (i in 0..10) {
+            a[i] = chars[i].code - '0'.code
+        }
+        if (a[0] == 0) return false
+        if (a[10] % 2 == 1) return false
+        if (((a[0] + a[2] + a[4] + a[6] + a[8]) * 7 - (a[1] + a[3] + a[5] + a[7])) % 10 != a[9]) return false
+        return (a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9]) % 10 == a[10]
     }
 }
+
+
